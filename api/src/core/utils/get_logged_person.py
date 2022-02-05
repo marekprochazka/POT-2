143c23693e91d8d426bfe09ec0bb33a9cbce2fd5
Marek Prochazka <m.prochazka2002@gmail.com>
2022-02-05 14:23:18 +0100

    api: refactoring type helpers

--- api/src/core/utils/get_logged_person.py
+++ api/src/core/utils/get_logged_person.py
@@ -1,7 +1,8 @@
 from core.models.person import Person
+from typing import Union
 
 
-def get_logged_person(request) -> (Person, None):
+def get_logged_person(request) -> Union[Person, None]:
     try:
         return request.user.person
     except AttributeError:
