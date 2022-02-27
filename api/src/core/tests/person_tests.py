48cd6de10ef087044e95862dc2ffca7c561a9b93
Marek Prochazka <m.prochazka2002@gmail.com>
2022-02-27 13:09:33 +0100

    api: tests

--- api/src/core/tests/person_tests.py
+++ api/src/core/tests/person_tests.py
@@ -1,4 +1,7 @@
+from rest_framework import status
+
 from core.tests.base import BaseTestCase
+from django.urls import reverse
 
 
 class PersonCreateTestCase(BaseTestCase):
@@ -9,3 +12,24 @@ class PersonCreateTestCase(BaseTestCase):
         self.assertNotEqual(self.person_1, None)
         self.assertNotEqual(self.person_2, None)
         self.assertEqual(self.person_none, None)
+
+    def test_person_upload_picture(self):
+        url = reverse('core:profile_picture')
+        response_1 = self.client.post(url)
+
+        self.login(self.person_0)
+        response_2 = self.client.post(url)
+
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response_1.status_code)
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response_2.status_code)
+
+    def test_person_get_picture(self):
+        url = reverse('core:profile_picture')
+        response_1 = self.client.get(url)
+
+        self.login(self.person_0)
+        response_2 = self.client.get(url)
+
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response_1.status_code)
+        self.assertEqual(status.HTTP_200_OK, response_2.status_code)
+        self.assertEqual(None, response_2.data.get('profile_picture'))
