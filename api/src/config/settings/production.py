d2295efb13a2b0288cd94a177fd3a838046c6a3f
Marek Prochazka <m.prochazka2002@gmail.com>
2022-06-09 14:12:35 +0200

    add sentry

--- api/src/config/settings/production.py
+++ api/src/config/settings/production.py
@@ -1,9 +1,16 @@
 from .base import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_DSN'],
+    integrations=[DjangoIntegration()]
+)
